342051cd325a58672462df58ac3bb8d4eae6f8c6
Nico Bazzoni <you@example.com>
2021-01-12 16:48:06 -0500

    add config file

---
+++ config/environment.rb
@@ -0,0 +1,8 @@
+
+require_relative '../lib/sightings.rb'
+require_relative '../lib/scraper.rb'
+require_relative '../lib/CLI.rb'
+require 'nokogiri'
+require 'open-uri'
+require 'pry'
+require 'colorize'
